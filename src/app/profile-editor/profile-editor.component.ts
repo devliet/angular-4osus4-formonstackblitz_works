696ec0c876391f071423496fba6f334b73c577b9
devliet <101347493+devliet@users.noreply.github.com>
2023-03-28 14:33:52 +0200

    2 files modified

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,17 +1,35 @@
 import { FormGroup, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['profile-editor.component.css'],
 })
 export class ProfileEditorComponent {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
   });
+
+  constructor(private fb: FormBuilder) {}
 
   onSubmit() {
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy R.',
+      address: { street: '123 Drew street' },
+    });
   }
 }
